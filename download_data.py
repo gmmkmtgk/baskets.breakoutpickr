40e1e1f3ad2fbd2379de010a36e119e66a6280a8
Keshav Maheshwari <keshavmaheshwari800@gmail.com>
2022-10-18 23:13:23 +0530

    h

--- download_data.py
+++ download_data.py
@@ -2,6 +2,6 @@ from process_data import download_bhavcopy_multiple_dates
 from datetime import date
 
 #download bhavcopy for the given dates
-start_date = date(2021, 8, 28)
-end_date = date(2022, 8, 28)
+start_date = date(2022, 8, 28)
+end_date = date(2022, 10, 8)
 download_bhavcopy_multiple_dates(start_date, end_date)
